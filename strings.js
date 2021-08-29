e2d739ac8fbb5311e3c99f61d7a8828b0ddb37ba
agatafigura <83277815+agatafigura@users.noreply.github.com>
2021-08-29 15:57:30 +0200

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const name = "Agata";
+const animal = "dog";
+const animalname = "Inka";
+
+const text = `My name is ${name}.
+I have a ${animal} called ${animalname}.`;
+
+console.log(text);
